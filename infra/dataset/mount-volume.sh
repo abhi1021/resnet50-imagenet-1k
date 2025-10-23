1c070bf655c97c91f4104956c07d036f04769477
Pandurang Patil <5101898+pandurangpatil@users.noreply.github.com>
2025-10-23 20:34:54 +0530

    Enhance mount-volume.sh with training command comments
    Added comments for training command and log monitoring.

--- infra/dataset/mount-volume.sh
+++ infra/dataset/mount-volume.sh
@@ -11,9 +11,14 @@ cd /mnt/imagenet
 source venv/bin/activate
 cd resnet50-imagenet-1k
 
+# Start the training, refer to the command in the message. Here it's for reference only
+python train.py --model resnet50-pytorch --dataset imagenet-1k --data-dir /mnt/imagenet/dataset --epochs 100 --batch-size 256 --scheduler onecycle --lr-finder --resume-from ./checkpoint_3 --hf-token <hf token> --hf-repo <hf repo> >> console-3.log  2>&1 &
 
 # watch GPU usage
 watch -n 0.01 nvidia-smi
 
 # monitor CPU and memory usage
 htop
+
+# To check the progress of the training, the console output is redirected to the console-3.log file.
+tail -f console-3.log 
